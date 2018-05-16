c10260e8d48352c524d77fcdb283aeba3559985c
manoj-k-mishra <manojmishra571980@gmail.com>
2018-05-16 13:32:27 +1000

    get all photo and get one photo working from server/graphql

--- lib/instagram_web/schema.ex
+++ lib/instagram_web/schema.ex
@@ -5,10 +5,18 @@ defmodule InstagramWeb.Schema do
   import_types __MODULE__.PostsTypes
 
   query do
+
     @desc "Get list of photo"
     field :photos, list_of(:photo) do
       resolve &Resolvers.Posts.photos/3
      end
+
+    @desc "get single photo"
+    field :photo, :photo do
+      arg :id, non_null(:id)
+      resolve &Resolvers.Posts.photo/3
+    end
+
   end
 
 end
