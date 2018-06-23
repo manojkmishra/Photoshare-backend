40cebfd1b5af68503b67440c45f7eb6f2778261c
manojkmishra <manojmishra571980@gmail.com>
2018-06-23 10:39:26 +1000

    mix ecto.migrate[like_photos]

--- priv/repo/migrations/20180622233750_create_like_photos.exs
+++ priv/repo/migrations/20180622233750_create_like_photos.exs
@@ -3,13 +3,14 @@ defmodule Instagram.Repo.Migrations.CreateLikePhotos do
 
   def change do
     create table(:like_photos) do
-      add :user_id, references(:users, on_delete: :nothing)
-      add :photo_id, references(:photos, on_delete: :nothing)
+      add :user_id, references(:users, on_delete: :nothing), null: false
+      add :photo_id, references(:photos, on_delete: :nothing), null: false
 
       timestamps()
     end
 
-    create index(:like_photos, [:user_id])
-    create index(:like_photos, [:photo_id])
+   # create index(:like_photos, [:user_id])
+   # create index(:like_photos, [:photo_id])
+   create unique_index(:like_photos, [:user_id, :photo_id], name: :user_like_photo)
   end
 end
